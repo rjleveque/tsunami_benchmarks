99417d3a7ef76d8bbd89cdf5aaf61318a1a8b8d2
Randy LeVeque <rjl@uw.edu>
2015-02-08 14:35:35 -0800

    problem2/from_japan1 set up to use UCSB source model

---
+++ nthmp_currents_2015/problem2/from_japan1/compare_gauges.py
@@ -0,0 +1,213 @@
+
+
+from pylab import *
+from clawpack.visclaw.data import ClawPlotData
+from clawpack.clawutil.data import ClawData
+
+comparison_data_dir = '../../all_data/2_Hilo_2011_Field/comparison_data/'
+
+geodata = ClawData()
+geodata.read('geoclaw.data', force=True)
+sea_level = geodata.sea_level
+print "GeoClaw simulation at sea_level = %g relative to MHW" % sea_level
+
+
+plotdata = ClawPlotData()
+
+plotdata.outdir = '_output'
+toffset = 1./6.  # hours
+#toffset = 6.6 # hours fits data at 7760 better
+
+figure(3333, figsize=(12,5))
+clf()
+
+# ---  Specified control point ---
+
+CP = loadtxt('../se.dat')
+CP_t = CP[:,0] / 60.  # hours since EQ
+CP_eta = CP[:,1] + 0.13 + sea_level # adjust to match geoclaw run
+
+g3333 = plotdata.getgauge(3333)
+t = g3333.t / 3600. + toffset
+
+
+plot(CP_t,CP_eta,'k-o',label='Specified')
+plot(t, g3333.q[3,:], 'r',label='GeoClaw')
+ylabel('meters')
+legend(loc='upper right')
+xlim(7.0,12.5)
+ylim(-1,1.5)
+title('Surface elevation at control point')
+show()
+
+
+# ---  Tide Gauge 7760 ---
+
+TG = loadtxt(comparison_data_dir + 'TG_1617760_detided.txt')
+TG_t = TG[:,0] / 3600.  # hours since EQ
+TG_eta = TG[:,1]
+
+for gaugeno in [7760, 7761, 7762]:
+    g= plotdata.getgauge(gaugeno)
+    t = g.t / 3600. + toffset
+    eta = g.q[3,:] - sea_level   # correct for sea_level to compare with detided
+
+    figure(gaugeno, figsize=(12,5))
+    clf()
+    plot(TG_t,TG_eta,'k-o',label='Observed')
+    plot(t, eta, 'r',label='GeoClaw')
+    ylabel('meters')
+    legend(loc='upper right')
+    xlim(7.0,12.5)
+    ylim(-2.5,2)
+    title('Tide Gauge gaugeno')
+    show()
+
+# ---  ADCP HAI1125 ---
+
+TG = loadtxt(comparison_data_dir + 'HAI1125_detided_harmonic.txt')
+TG_t = TG[:,0]  # hours since EQ
+TG_u = TG[:,1]
+TG_v = TG[:,2]
+
+g = plotdata.getgauge(1125)
+t = g.t / 3600. + toffset
+u = g.q[1,:] / g.q[0,:]  * 100 # convert to cm/sec
+v = g.q[2,:] / g.q[0,:]  * 100 # convert to cm/sec
+
+figure(1125, figsize=(12,12))
+clf()
+subplot(311)
+plot(TG_t,TG_u,'k-o',label='Observed')
+plot(t, u, 'r',label='GeoClaw')
+ylabel('cm/sec')
+legend(loc='upper right')
+xlim(7.0,12.5)
+ylim(-150,150)
+title('HAI1125, E-W velocity')
+
+subplot(312)
+plot(TG_t,TG_v,'k-o',label='Observed')
+plot(t, v, 'r',label='GeoClaw')
+ylabel('cm/sec')
+legend(loc='upper right')
+xlim(7.0,12.5)
+ylim(-150,150)
+title('HAI1125, N-S velocity')
+
+subplot(313)
+TG_s = sqrt(TG_u**2 + TG_v**2)
+s = sqrt(u**2 + v**2)
+plot(TG_t,TG_s,'k-o',label='Observed')
+plot(t, s, 'r',label='GeoClaw')
+ylabel('cm/sec')
+legend(loc='upper right')
+xlim(7.0,12.5)
+ylim(-50,150)
+title('HAI1125, speed')
+
+show()
+
+# ---  ADCP HAI1126 ---
+
+TG = loadtxt(comparison_data_dir + 'HAI1126_detided_harmonic.txt')
+TG_t = TG[:,0]  # hours since EQ
+TG_u = TG[:,1]
+TG_v = TG[:,2]
+
+g = plotdata.getgauge(1126)
+t = g.t / 3600. + toffset
+u = g.q[1,:] / g.q[0,:]  * 100 # convert to cm/sec
+v = g.q[2,:] / g.q[0,:]  * 100 # convert to cm/sec
+
+figure(1126, figsize=(12,12))
+clf()
+subplot(311)
+plot(TG_t,TG_u,'k-o',label='Observed')
+plot(t, u, 'r',label='GeoClaw')
+ylabel('cm/sec')
+legend(loc='upper right')
+xlim(7.0,12.5)
+ylim(-150,150)
+title('HAI1126, E-W velocity')
+
+subplot(312)
+plot(TG_t,TG_v,'k-o',label='Observed')
+plot(t, v, 'r',label='GeoClaw')
+ylabel('cm/sec')
+legend(loc='upper right')
+xlim(7.0,12.5)
+ylim(-150,150)
+title('HAI1126, N-S velocity')
+
+subplot(313)
+TG_s = sqrt(TG_u**2 + TG_v**2)
+s = sqrt(u**2 + v**2)
+plot(TG_t,TG_s,'k-o',label='Observed')
+plot(t, s, 'r',label='GeoClaw')
+ylabel('cm/sec')
+legend(loc='upper right')
+xlim(7.0,12.5)
+ylim(-50,150)
+title('HAI1126, speed')
+show()
+
+# --- S2 near  ADCP HAI1126 ---
+
+TG = loadtxt(comparison_data_dir + 'HAI1126_detided_harmonic.txt')
+TG_t = TG[:,0]  # hours since EQ
+TG_u = TG[:,1]
+TG_v = TG[:,2]
+
+g = plotdata.getgauge(2222)
+t = g.t / 3600. + toffset
+u = g.q[1,:] / g.q[0,:]  * 100 # convert to cm/sec
+v = g.q[2,:] / g.q[0,:]  * 100 # convert to cm/sec
+
+figure(2222, figsize=(12,12))
+clf()
+subplot(311)
+plot(TG_t,TG_u,'k-o',label='Observed')
+plot(t, u, 'r',label='GeoClaw')
+ylabel('cm/sec')
+legend(loc='upper right')
+xlim(7.0,12.5)
+ylim(-150,150)
+title('S2, E-W velocity')
+
+subplot(312)
+plot(TG_t,TG_v,'k-o',label='Observed')
+plot(t, v, 'r',label='GeoClaw')
+ylabel('cm/sec')
+legend(loc='upper right')
+xlim(7.0,12.5)
+ylim(-150,150)
+title('S2, N-S velocity')
+
+subplot(313)
+TG_s = sqrt(TG_u**2 + TG_v**2)
+s = sqrt(u**2 + v**2)
+plot(TG_t,TG_s,'k-o',label='Observed')
+plot(t, s, 'r',label='GeoClaw')
+ylabel('cm/sec')
+legend(loc='upper right')
+xlim(7.0,12.5)
+ylim(-50,150)
+title('S2, speed')
+show()
+
+
+def save_plot(gaugeno):
+    fname = 'gauge%s.png' % gaugeno
+    figure(gaugeno)
+    savefig(fname)
+    print "Created ",fname
+
+if 1:
+    save_plot(3333)
+    save_plot(7760)
+    #save_plot(7761)
+    #save_plot(7762)
+    save_plot(1125)
+    save_plot(1126)
+    save_plot(2222)
---
+++ nthmp_currents_2015/problem2/from_japan1/setplot.py
@@ -0,0 +1,481 @@
+
+""" 
+Set up the plot figures, axes, and items to be done for each frame.
+
+This module is imported by the plotting routines and then the
+function setplot is called to set the plot parameters.
+    
+""" 
+
+from clawpack.geoclaw import topotools
+import numpy as np
+
+
+#--------------------------
+def setplot(plotdata):
+#--------------------------
+    
+    """ 
+    Specify what is to be plotted at each frame.
+    Input:  plotdata, an instance of pyclaw.plotters.data.ClawPlotData.
+    Output: a modified version of plotdata.
+    
+    """ 
+
+
+    from clawpack.visclaw import colormaps, geoplot
+    from numpy import linspace
+
+    plotdata.clearfigures()  # clear any old figures,axes,items data
+    plotdata.format = 'binary'
+
+
+    # To plot gauge locations on pcolor or contour plot, use this as
+    # an afteraxis function:
+
+    def addgauges(current_data):
+        from clawpack.visclaw import gaugetools
+        gaugetools.plot_gauge_locations(current_data.plotdata, \
+             gaugenos='all', format_string='ko', add_labels=False)
+    
+    def fixup(current_data):
+        import pylab
+        #addgauges(current_data)
+        t = current_data.t
+        t = t / 3600.  # hours
+        pylab.title('Surface at %4.2f hours' % t, fontsize=20)
+        pylab.ticklabel_format(format='plain',useOffset=False)
+        mean_lat = 19.7
+        pylab.gca().set_aspect(1.0 / pylab.cos(pylab.pi / 180.0 * mean_lat))
+        #pylab.xticks(fontsize=15)
+        #pylab.yticks(fontsize=15)
+
+
+    #-----------------------------------------
+    # Figure for imshow plot
+    #-----------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='Pacific', figno=1)
+    #plotfigure.show = False
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes('imshow')
+    plotaxes.title = 'Surface'
+    #plotaxes.scaled = True
+
+    plotaxes.afteraxes = fixup
+
+    # Water
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    # plotitem.plot_var = geoplot.surface
+    plotitem.plot_var = geoplot.surface_or_depth
+    plotitem.imshow_cmap = geoplot.tsunami_colormap
+    plotitem.imshow_cmin = -0.2
+    plotitem.imshow_cmax = 0.2
+    plotitem.add_colorbar = True
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 0
+
+    # Land
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    plotitem.plot_var = geoplot.land
+    plotitem.imshow_cmap = geoplot.land_colors
+    plotitem.imshow_cmin = 0.0
+    plotitem.imshow_cmax = 100.0
+    plotitem.add_colorbar = False
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 0
+    plotaxes.xlimits = [135,210]
+    plotaxes.ylimits = [9,53]
+
+    # add contour lines of bathy if desired:
+    plotitem = plotaxes.new_plotitem(plot_type='2d_contour')
+    plotitem.show = False
+    plotitem.plot_var = geoplot.topo
+    plotitem.contour_levels = linspace(-2000,0,5)
+    plotitem.amr_contour_colors = ['y']  # color on each level
+    plotitem.kwargs = {'linestyles':'solid','linewidths':2}
+    plotitem.amr_contour_show = [1,0,0]  
+    plotitem.celledges_show = 0
+    plotitem.patchedges_show = 0
+
+
+    #-----------------------------------------
+    # Figure for zoom plot
+    #-----------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='Hawaii', figno=20)
+    #plotfigure.show = False
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes('imshow')
+    plotaxes.title = 'Surface'
+    #plotaxes.scaled = True
+
+    plotaxes.afteraxes = fixup
+
+    # Water
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    # plotitem.plot_var = geoplot.surface
+    plotitem.plot_var = geoplot.surface_or_depth
+    plotitem.imshow_cmap = geoplot.tsunami_colormap
+    plotitem.imshow_cmin = -0.2
+    plotitem.imshow_cmax = 0.2
+    plotitem.add_colorbar = True
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 0
+
+    # Land
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    plotitem.plot_var = geoplot.land
+    plotitem.imshow_cmap = geoplot.land_colors
+    plotitem.imshow_cmin = 0.0
+    plotitem.imshow_cmax = 100.0
+    plotitem.add_colorbar = False
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 0
+    plotaxes.xlimits = [200., 208.]
+    plotaxes.ylimits = [17, 25]
+
+
+
+    #-----------------------------------------
+    # Figure for zoom plot
+    #-----------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='zoom', figno=2)
+    plotfigure.show = False
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes('imshow')
+    plotaxes.title = 'Surface'
+    #plotaxes.scaled = True
+
+    plotaxes.afteraxes = fixup
+
+    # Water
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    # plotitem.plot_var = geoplot.surface
+    plotitem.plot_var = geoplot.surface_or_depth
+    plotitem.imshow_cmap = geoplot.tsunami_colormap
+    plotitem.imshow_cmin = -0.2
+    plotitem.imshow_cmax = 0.2
+    plotitem.add_colorbar = True
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 0
+
+    # Land
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    plotitem.plot_var = geoplot.land
+    plotitem.imshow_cmap = geoplot.land_colors
+    plotitem.imshow_cmin = 0.0
+    plotitem.imshow_cmax = 100.0
+    plotitem.add_colorbar = False
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 0
+    plotaxes.xlimits = [204.8, 205.]
+    plotaxes.ylimits = [19.7, 19.9]
+
+
+    #-----------------------------------------
+    # Figure for zoom plot
+    #-----------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='Hilo', figno=3)
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes('imshow')
+    plotaxes.title = 'Surface'
+    #plotaxes.scaled = True
+
+    plotaxes.afteraxes = fixup
+
+
+    # Water
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    #plotitem.show = False
+    # plotitem.plot_var = geoplot.surface
+    plotitem.plot_var = geoplot.surface_or_depth
+    plotitem.imshow_cmap = geoplot.tsunami_colormap
+    plotitem.imshow_cmin = -0.5
+    plotitem.imshow_cmax = 0.5
+    plotitem.add_colorbar = True
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 0
+
+    # Land
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    plotitem.plot_var = geoplot.land
+    plotitem.imshow_cmap = geoplot.land_colors
+    plotitem.imshow_cmin = 0.0
+    plotitem.imshow_cmax = 100.0
+    plotitem.add_colorbar = False
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 0
+    plotaxes.xlimits = [204.9, 204.96]
+    plotaxes.ylimits = [19.72, 19.76]
+
+
+    # add contour lines of bathy if desired:
+    plotitem = plotaxes.new_plotitem(plot_type='2d_contour')
+    plotitem.show = False
+    plotitem.plot_var = geoplot.topo
+    plotitem.contour_levels = linspace(-2000,0,5)
+    plotitem.amr_contour_colors = ['y']  # color on each level
+    plotitem.kwargs = {'linestyles':'solid','linewidths':2}
+    plotitem.amr_contour_show = [1,0,0]  
+    plotitem.celledges_show = 0
+    plotitem.patchedges_show = 0
+
+    #-----------------------------------------
+    # Figure for speed
+    #-----------------------------------------
+
+    plotfigure = plotdata.new_plotfigure(name='speed1', figno=4)
+    plotfigure.show = False
+
+    def speed(current_data):
+        from numpy import ma, where, sqrt
+        drytol = 1e-3
+        q = current_data.q
+        h = q[0,:,:]
+        hu = q[1,:,:]
+        hv = q[2,:,:]
+        speed = where(h > drytol, sqrt(hu**2 + hv**2)/h**2, 0.)
+        speed = ma.masked_where(h<=drytol, speed)
+        return speed
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes()
+    plotaxes.title = 'Speed'
+    #plotaxes.scaled = True
+
+    def fixup(current_data):
+        import pylab
+        #addgauges(current_data)
+        t = current_data.t
+        t = t / 3600.  # hours
+        pylab.title('Speed at %4.2f hours' % t, fontsize=20)
+        pylab.ticklabel_format(format='plain',useOffset=False)
+        mean_lat = 19.7
+        pylab.gca().set_aspect(1.0 / pylab.cos(pylab.pi / 180.0 * mean_lat))
+
+    plotaxes.afteraxes = fixup
+
+    # Water (contourf not working)
+    plotitem = plotaxes.new_plotitem(plot_type='2d_contourf')
+    plotitem.show = False
+    plotitem.plot_var = speed
+    clines = [0, 0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.2]
+    plotitem.contour_levels = clines
+    from clawpack.geoclaw.geoplot import discrete_cmap_1
+    plotitem.fill_colors = discrete_cmap_1(clines)
+    plotitem.kwargs = {'extend':'max'}
+    plotitem.add_colorbar = True
+    plotitem.amr_celledges_show = 0
+    plotitem.patchedges_show = 0
+
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    plotitem.plot_var = speed
+    plotitem.imshow_cmap = geoplot.tsunami_colormap
+    plotitem.imshow_cmin = 0.0
+    plotitem.imshow_cmax = 0.3
+    plotitem.add_colorbar = True
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 0
+
+    # Land
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    #plotitem.show = False
+    plotitem.plot_var = geoplot.land
+    plotitem.imshow_cmap = geoplot.land_colors
+    plotitem.imshow_cmin = 0.0
+    plotitem.imshow_cmax = 100.0
+    plotitem.add_colorbar = False
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 0
+    plotaxes.xlimits = [204.9, 204.96]
+    plotaxes.ylimits = [19.72, 19.76]
+
+
+    # add contour lines of bathy if desired:
+    plotitem = plotaxes.new_plotitem(plot_type='2d_contour')
+    plotitem.show = False
+    plotitem.plot_var = geoplot.topo
+    plotitem.contour_levels = linspace(-2000,0,5)
+    plotitem.amr_contour_colors = ['y']  # color on each level
+    plotitem.kwargs = {'linestyles':'solid','linewidths':2}
+    plotitem.amr_contour_show = [1,0,0]  
+    plotitem.celledges_show = 0
+    plotitem.patchedges_show = 0
+
+    #-----------------------------------------
+    # Figure for velocity plot
+    #-----------------------------------------
+    
+    plotfigure = plotdata.new_plotfigure(name='Speed', figno=10)
+    #plotfigure.show = False
+
+    # Set up for axes for velocity
+    plotaxes = plotfigure.new_plotaxes()
+    #plotaxes.axescmd = 'subplot(212)'
+    plotaxes.title = 'Speed'
+    plotaxes.scaled = True
+
+    def fixup(current_data):
+        import pylab
+        addgauges(current_data)
+        t = current_data.t
+        t = t / 3600.  # hours
+        pylab.title('Speed at %4.2f hours' % t, fontsize=20)
+        pylab.ticklabel_format(format='plain',useOffset=False)
+        mean_lat = 19.7
+        pylab.gca().set_aspect(1.0 / pylab.cos(pylab.pi / 180.0 * mean_lat))
+        pylab.xticks(rotation=20)
+    plotaxes.afteraxes = fixup
+    plotaxes.xlimits = [204.9, 204.96]
+    plotaxes.ylimits = [19.72, 19.76]
+
+    def speed(current_data):
+        from pylab import where,sqrt
+        q = current_data.q
+        h = q[0,:,:]
+        dry_tol = 0.001
+        u = where(h>dry_tol, q[1,:,:]/h, 0.)
+        v = where(h>dry_tol, q[2,:,:]/h, 0.)
+        s = sqrt(u**2 + v**2)
+        return s
+
+
+    # Water
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    plotitem.plot_var = speed
+    #plotitem.imshow_cmap = colormaps.white_red
+    #plotitem.imshow_cmap = colormaps.yellow_red_blue
+    plotitem.imshow_cmap = \
+           colormaps.make_colormap({0:[1,1,1],0.5:[0.1,0.1,1],1:[1,0.1,0.1]})
+    plotitem.imshow_cmin = 0.
+    plotitem.imshow_cmax = 1.0
+    plotitem.add_colorbar = True
+    plotitem.colorbar_shrink = 0.6
+    plotitem.amr_celledges_show = [0]
+    plotitem.amr_patchedges_show = [0]
+
+
+    #-----------------------------------------
+    # Figures for gauges
+    #-----------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='Surface', figno=300, \
+                    type='each_gauge')
+    plotfigure.clf_each_gauge = True
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes()
+    #plotaxes.axescmd = 'subplot(2,1,1)'
+    plotaxes.title = 'Surface'
+
+    # Plot surface as blue curve:
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    plotitem.plot_var = 3
+    plotitem.plotstyle = 'b-'
+    plotitem.kwargs = {'linewidth':2}
+
+    # Plot topo as green curve:
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    plotitem.show = False
+
+    def gaugetopo(current_data):
+        q = current_data.q
+        h = q[0,:]
+        eta = q[3,:]
+        topo = eta - h
+        return topo
+        
+    plotitem.plot_var = gaugetopo
+    plotitem.plotstyle = 'g-'
+
+    def add_zeroline(current_data):
+        from pylab import plot, legend, xticks, floor, xlim,ylim
+        t = current_data.t
+        #legend(('surface','topography'),loc='lower left')
+        plot(t, 0*t, 'k')
+        n = int(floor(t.max()/1800.)) + 2
+        xticks([1800*i for i in range(n)],[str(0.5*i) for i in range(n)])
+        xlim(25000,t.max())
+        #ylim(-0.5,0.5)
+        print "+++ gaugeno = ",current_data.gaugeno
+
+    #plotaxes.ylimits = [-0.5, 0.5]
+    plotaxes.afteraxes = add_zeroline
+
+
+    plotfigure = plotdata.new_plotfigure(name='Velocities', figno=301, \
+                    type='each_gauge')
+    plotfigure.clf_each_gauge = True
+
+    plotaxes = plotfigure.new_plotaxes()
+    #plotaxes.axescmd = 'subplot(2,1,2)'
+    plotaxes.title = 'Velocities'
+    #plotaxes.afteraxes = add_zeroline
+
+    # Plot velocity as red curve:
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    plotitem.show = False
+    def speed(current_data):
+        from numpy import where, sqrt
+        h = current_data.q[0,:]
+        h = where(h>0.01, h, 1.e6)
+        u = 100. * current_data.q[1,:] / h
+        v = 100. * current_data.q[2,:] / h
+        s = sqrt(u**2 + v**2)
+        return s
+    plotitem.plot_var = speed
+    plotitem.plotstyle = 'k-'
+
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    def uvel(current_data):
+        from numpy import where, sqrt
+        h = current_data.q[0,:]
+        h = where(h>0.01, h, 1.e6)
+        u = 100. * current_data.q[1,:] / h
+        return u
+    plotitem.plot_var = uvel
+    plotitem.plotstyle = 'r-'
+    plotitem.kwargs = {'linewidth':2}
+
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    def vvel(current_data):
+        from numpy import where, sqrt
+        h = current_data.q[0,:]
+        h = where(h>0.01, h, 1.e6)
+        v = 100. * current_data.q[2,:] / h
+        return v
+    plotitem.plot_var = vvel
+    plotitem.plotstyle = 'g-'
+    plotitem.kwargs = {'linewidth':2}
+
+    def add_legend(current_data):
+        from pylab import legend
+        legend(['u','v'],'upper left')
+        #legend(['Speed','u','v'],'upper left')
+        add_zeroline(current_data)
+    plotaxes.ylimits = [-50,50]
+    plotaxes.afteraxes = add_legend
+
+
+
+    #-----------------------------------------
+    
+    # Parameters used only when creating html and/or latex hardcopy
+    # e.g., via pyclaw.plotters.frametools.printframes:
+
+    plotdata.printfigs = True                # print figures
+    plotdata.print_format = 'png'            # file format
+    plotdata.print_framenos = 'all'          # list of frames to print
+    plotdata.print_gaugenos = 'all'          # list of gauges to print
+    plotdata.print_fignos = 'all'            # list of figures to print
+    plotdata.html = True                     # create html files of plots?
+    plotdata.html_homelink = '../README.html'   # pointer for top of index
+    plotdata.latex = True                    # create latex file of plots?
+    plotdata.latex_figsperline = 2           # layout of plots
+    plotdata.latex_framesperline = 1         # layout of plots
+    plotdata.latex_makepdf = False           # also run pdflatex?
+
+    return plotdata
+
